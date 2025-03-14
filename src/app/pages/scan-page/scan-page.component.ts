2769881ae1528f855e4e5219c1280af076e489a5
Gabriel Freitas <gabrielfreitas@pwrgestao.com>
2025-03-13 23:31:13 -0300

    feat: lanterna

--- src/app/pages/scan-page/scan-page.component.ts
+++ src/app/pages/scan-page/scan-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-scan-page',
-  imports: [],
+  imports: [ZXingScannerModule, MatButtonModule, MatIconModule],
   templateUrl: './scan-page.component.html',
-  styleUrl: './scan-page.component.css'
+  styleUrl: './scan-page.component.css',
 })
 export class ScanPageComponent {
+  readonly allowedFormats: BarcodeFormat[] = [
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+  ];
+  flashlightCompativel?: boolean;
+  flashlightStatus = false;
+  camerasFound: MediaDeviceInfo[] = [];
 
+  handleScanCodigo(codigo: string) {
+    console.log('Codigo lido com sucesso: ', codigo);
+  }
 }
